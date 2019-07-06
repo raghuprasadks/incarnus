989c31b5e4049ee71c9d6b055a8d438538608d3c
raghu prasad <prasadraghuks@gmail.com>
2019-07-06 17:15:44 +0530

    added shopping cart app

---
+++ workingsamples/mean/angular/ShoppingCart/js/controller.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+// the storeController contains two objects:
+// - store: contains the product list
+// - cart: the shopping cart object
+function storeController($scope, $routeParams, DataService) {
+
+    // get store and cart from service
+    $scope.store = DataService.store;
+    $scope.cart = DataService.cart;
+
+    // apply changes when cart items change
+    $scope.cart.itemsChanged = function (e) {
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    }
+
+    // use routing to pick the selected product
+    if ($routeParams.productSku != null) {
+        $scope.product = $scope.store.getProduct($routeParams.productSku);
+    }
+}
