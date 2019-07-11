b68d74eedae8e1366f079accb27525db495f1f98
raghu prasad <prasadraghuks@gmail.com>
2019-07-11 13:12:06 +0530

    registration app

---
+++ meanapp/registrationapp/controllers/app.controller.js
@@ -0,0 +1,24 @@
+﻿var express = require('express');
+var router = express.Router();
+
+// use session auth to secure the angular app files
+router.use('/', function (req, res, next) {
+	console.log('token : ',req.session.token);
+	console.log('path :',req.path);
+    if (req.path !== '/login' && !req.session.token) {
+        return res.redirect('/login?returnUrl=' + encodeURIComponent('/app' + req.path));
+    }
+
+    next();
+});
+
+// make JWT token available to angular app
+router.get('/token', function (req, res) {
+	 console.log('jwt token : ',req.session.token);
+    res.send(req.session.token);
+});
+
+// serve angular app files from the '/app' route
+router.use('/', express.static('app'));
+
+module.exports = router;
